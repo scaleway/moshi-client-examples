10f792f728a0c3f3a767160134be031d1466ffbc
Benoit Marzelleau <bmarzelleau@scaleway.com>
2024-10-29 14:56:16 +0100

    Remove useless environement variable

--- web-ui/src/env.ts
+++ web-ui/src/env.ts
@@ -1,21 +1,14 @@
 type ENV = {
-  VITE_QUEUE_API_PATH: string;
   VITE_API_URL: string;
   VITE_SECURE: string;
   VITE_ENV: 'development' | 'production';
 };
 
 const parseEnv = (): ENV => {
-  const VITE_QUEUE_API_PATH = import.meta.env.VITE_QUEUE_API_PATH;
   const VITE_API_URL = import.meta.env.VITE_API_URL;
   const VITE_SECURE = import.meta.env.VITE_SECURE;
   
-  if (!VITE_QUEUE_API_PATH) {
-    throw new Error("VITE_QUEUE_API_PATH is not defined");
-  }
-
   return {
-    VITE_QUEUE_API_PATH,
     VITE_API_URL,
     VITE_SECURE,
     VITE_ENV: import.meta.env.DEV ? 'development' : 'production',
