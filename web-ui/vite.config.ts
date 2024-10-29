456e3913b59669849212bcd0116220081a72c032
Benoit Marzelleau <bmarzelleau@scaleway.com>
2024-10-29 14:54:40 +0100

    Cleanup vite config

--- web-ui/vite.config.ts
+++ web-ui/vite.config.ts
@@ -1,21 +1,8 @@
-import { ProxyOptions, defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import topLevelAwait from "vite-plugin-top-level-await";
 
-export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, process.cwd());
-  const proxyConf:Record<string, string | ProxyOptions> = env.VITE_QUEUE_API_URL ? {
-    "/api": {
-      target: env.VITE_QUEUE_API_URL,
-      changeOrigin: true,
-    },
-  } : {};
+export default defineConfig(() => {
   return {
-    server: {
-      host: "0.0.0.0",
-      proxy:{
-        ...proxyConf,
-      }
-    },
     plugins: [
       topLevelAwait({
         // The export name of top-level await promise for each chunk module
